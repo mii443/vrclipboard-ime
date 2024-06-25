a3a2de5e27a0421f386a79324faca1806f2b4545
mii <mii@sfc.wide.ad.jp>
2024-06-25 19:39:15 +0900

    default config generate

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,4 @@
-use std::{fs::File, io::Read};
+use std::{fs::File, io::{Read, Write}, path::Path};
 
 use serde_derive::Deserialize;
 use anyhow::Result;
@@ -12,10 +12,22 @@ pub struct Config {
 
 impl Config {
     pub fn load() -> Result<Config> {
-        let mut file = File::open("config.yaml")?;
+        if !Path::new("./config.yaml").exists() {
+            Self::generate_default_config()?;
+        }
+        let mut file = File::open("./config.yaml")?;
         let mut contents = String::new();
         file.read_to_string(&mut contents)?;
         let config: Config = serde_yaml::from_str(&contents)?;
         Ok(config)
+    }
+
+    pub fn generate_default_config() -> Result<()> {
+        let mut file = File::create("./config.yaml")?;
+        file.write_all(br#"prefix: ";"
+command: ";"
+split: "/""#)?;
+        file.flush()?;
+        Ok(())
     }
 }
